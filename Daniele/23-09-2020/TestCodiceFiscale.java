acff60a4e45858892892014c4abb5df1e5f1bd18
VittoreD <71709805+VittoreD@users.noreply.github.com>
2020-09-24 18:42:00 +0200

    Add files via upload

--- Daniele/23-09-2020/TestCodiceFiscale.java
+++ Daniele/23-09-2020/TestCodiceFiscale.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
+package codiceFiscale;
+
+import metodiUtility.*;
 
 public class TestCodiceFiscale {
 
 	public static void main(String[] args) {
-		Scanner tastiera=new Scanner(System.in);
-		
-		System.out.println("Inserisci codice fiscale: ");
-		String cf=tastiera.next();
-		cf=cf.toUpperCase();
-		CodiceFiscale utente=new CodiceFiscale(cf);
-		if(utente.isValid()) {
+
+		CodiceFiscale utente = new CodiceFiscale(
+				MetodiAusiliari.inputStringa("Inserisci Codice Fiscale").toUpperCase());
+		if (utente.isValid()) {
 			System.out.println(utente);
 		} else
 			System.out.println("Codice fiscale non valido");
-		
-		
-		tastiera.close();
-		
+
 	}
 
 }
