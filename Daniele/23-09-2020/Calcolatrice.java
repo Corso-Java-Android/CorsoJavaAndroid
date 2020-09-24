449ceeac1e2dff553ec3315804eeb00b76dc3d5d
VittoreD <71709805+VittoreD@users.noreply.github.com>
2020-09-24 18:41:26 +0200

    Add files via upload

--- Daniele/23-09-2020/Calcolatrice.java
+++ Daniele/23-09-2020/Calcolatrice.java
@@ -1,33 +1,29 @@
-/*
- * Autore: Daniele Vittore
- * Data: 23/09/2020
- * Versione: 0.1
- * 
- */
+package calcolatrice;
+
 import java.lang.System;
+
 public class Calcolatrice {
 
 	public double somma(double operandoUno, double operandoDue) {
-		return operandoUno+operandoDue;
+		return operandoUno + operandoDue;
 	}
-	
+
 	public double sottrazione(double operandoUno, double operandoDue) {
-		return operandoUno-operandoDue;
+		return operandoUno - operandoDue;
 	}
 
 	public double moltiplicazione(double operandoUno, double operandoDue) {
-		return operandoUno*operandoDue;
+		return operandoUno * operandoDue;
 	}
-	
+
 	public double divisione(double operandoUno, double operandoDue) {
-		if(operandoDue==0) {
+		if (operandoDue == 0) {
 			System.out.print("Divisione per zero! ");
 			return -1;
 		} else {
-			return operandoUno/operandoDue;
+			return operandoUno / operandoDue;
 		}
 
 	}
-	
-	
+
 }
--- Daniele/23-09-2020/TestCalcolatrice.java
+++ Daniele/23-09-2020/TestCalcolatrice.java
@@ -1,24 +1,25 @@
+package calcolatrice;
+
 import java.util.Scanner;
 
 public class TestCalcolatrice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Calcolatrice calc= new Calcolatrice();
-		
-		Scanner tastiera=new Scanner(System.in);
-		
+		Calcolatrice calc = new Calcolatrice();
+
+		Scanner tastiera = new Scanner(System.in);
+
 		System.out.println("Inserire primo operando: ");
-		double operandoUno=tastiera.nextDouble();
-		
+		double operandoUno = tastiera.nextDouble();
+
 		System.out.println("Inserire secondo operando: ");
-		double operandoDue=tastiera.nextDouble();
-		
-		System.out.println("La somma è: "+ calc.somma(operandoUno, operandoDue));
-		System.out.println("La differenza è: "+ calc.sottrazione(operandoUno, operandoDue));
-		System.out.println("La moltiplicazione è: "+ calc.moltiplicazione(operandoUno, operandoDue));
-		System.out.println("La divisione è: "+ calc.divisione(operandoUno, operandoDue));
-		
+		double operandoDue = tastiera.nextDouble();
+
+		System.out.println("La somma è: " + calc.somma(operandoUno, operandoDue));
+		System.out.println("La differenza è: " + calc.sottrazione(operandoUno, operandoDue));
+		System.out.println("La moltiplicazione è: " + calc.moltiplicazione(operandoUno, operandoDue));
+		System.out.println("La divisione è: " + calc.divisione(operandoUno, operandoDue));
+
 		tastiera.close();
 	}
 
