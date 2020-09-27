ffe31f91ce9dd10d0349b6acaaf186639d193614
SaraVizz <71710209+SaraVizz@users.noreply.github.com>
2020-09-27 20:46:40 +0200

    Create MainBib.java

---
+++ Sara/25_Settembre/biblioteca/MainBib.java
@@ -0,0 +1,11 @@
+public class MainBib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Libro l=new Libro("Masfield Park", "Jane Austen");
+     l.setPrezzo(10.99);
+     l.setEditore("Bompiani");
+     l.stampa();
+	}
+
+}
