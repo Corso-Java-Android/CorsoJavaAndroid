909bc9841670d38370794bc4b0b0a4302a34fb9b
SaraVizz <71710209+SaraVizz@users.noreply.github.com>
2020-09-27 20:41:10 +0200

    Create Quadrato.java

---
+++ Sara/25_Settembre/Figure/Quadrato.java
@@ -0,0 +1,11 @@
+public class Quadrato extends Rettangolo {
+	
+	public Quadrato(double lato){
+		super(lato,lato);
+	}
+	
+	public String toString(){
+		return "Quadrato [lato="+super.base+"]";
+	}
+
+}
