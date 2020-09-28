9b4121ebd38445218417779430da3503d8222467
Vincenzo <v.santoro21@studenti.unisa.it>
2020-09-28 11:04:50 +0200

    28 Settembre

---
+++ Vincenzo/OrdineCasualeGenerator.java
@@ -0,0 +1,24 @@
+package casuale;
+
+import java.util.Random;
+
+public class OrdineCasualeGenerator {
+	String[] nominativi;
+	
+	public OrdineCasualeGenerator(String[] nominativi) {
+		this.nominativi = nominativi;
+	}
+	
+	public String[] nominativi() {
+		Random rand = new Random(System.currentTimeMillis());
+		for(int i=0;i<nominativi.length;i++) {
+			int index = rand.nextInt(nominativi.length);	//genero un indice casuale
+			String temp = nominativi[index];	//salvo il contenuto di index in una var. temporanea
+			nominativi[index] = nominativi[i];	//inserisco il contenuto di i in index
+			nominativi[i] = temp;	//inserisco il contenuto di index in i tramite la var. temporanea
+		}
+		return nominativi;
+	}
+	
+
+}
---
+++ Vincenzo/OrdineTest.java
@@ -0,0 +1,19 @@
+package casuale;
+
+public class OrdineTest {
+
+	public static void main(String[] args) {
+		String[] nominativi = {"Alessio Angelo Liotta", "Davide Pasquali", "Enrico Macrì", "Tommaso Segato", "Daniele Vittore", "Danis Moretta", "Erika Corallo", "Gabriele Lo Piccolo", "Giovanni Battista Cortese", "Karol Khaira", "Olivia Nadia Djeugove", "Sara Vizzaclaro", "Valentina De Miglio", "Vincenzo Santoro"};
+		OrdineCasualeGenerator generatore = new OrdineCasualeGenerator(nominativi);
+		System.out.println("Ci sono "+nominativi.length+" nominativi");
+		for(int i=0;i<nominativi.length;i++)	//nominativi in ordine
+			System.out.println(nominativi[i]);
+		System.out.println("===============");
+		String[] nominativi2 = generatore.nominativi();	//ordinamento casuale
+		System.out.println("Ci sono "+nominativi2.length+" nominativi");
+		for(int i=0;i<nominativi2.length;i++)	//nominativi in ordine casuale
+			System.out.println(nominativi2[i]);
+
+	}
+
+}
