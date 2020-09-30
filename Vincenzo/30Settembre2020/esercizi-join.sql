1df287b619b5ee3b8e0ea4dbb46b1b5b305b81c3
Vincenzo <v.santoro21@studenti.unisa.it>
2020-09-30 12:31:56 +0200

    Update and rename inner-join.sql to esercizi-join.sql

--- Vincenzo/30Settembre2020/esercizi-join.sql
+++ Vincenzo/30Settembre2020/esercizi-join.sql
@@ -11,3 +11,23 @@ p.id_regione, r.nome, p.id, p.nome, p.sigla_automobilistica
 from province p, regioni r 
 where r.nome = 'Lazio' and p.id_regione = r.id
 ;
+
+select count(*) from comuni where id_provincia = 70;
+
+select * from province where id=70;
+
+-- quale è il nome della provincia 70 ovvero nome provincia, numero comuni
+select p.nome, count(*) as numero_comuni
+from comuni c, province p
+where c.id_provincia = 70 AND p.id = 70
+;
+
+select * from regioni where nome = 'Puglia';
+
+select count(*) from comuni where id_regione = 16;
+
+-- quanti sono i comuni della puglia
+select count(*)
+from comuni c, regioni r
+where c.id_regione = r.id AND r.nome = 'Puglia'
+;
