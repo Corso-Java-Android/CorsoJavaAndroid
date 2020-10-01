88685fc39edc3b97cf71871ef6fb7d81b73147cc
Vincenzo <v.santoro21@studenti.unisa.it>
2020-10-01 10:22:34 +0200

    Update 01/10/2020

---
+++ Vincenzo/30Settembre2020/database/src/database/test01/FirstConnection.java
@@ -0,0 +1,11 @@
+package database.test01;
+
+public class FirstConnection {
+
+	public static void main(String[] args) {
+		String driver = "org.mariadb.jdbc.Driver";
+		System.out.println(driver);
+
+	}
+
+}
