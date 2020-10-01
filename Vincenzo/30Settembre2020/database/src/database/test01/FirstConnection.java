d4b07d4b3932e283c0f6566835c25fd4d6d1c866
Vincenzo <v.santoro21@studenti.unisa.it>
2020-10-01 10:51:20 +0200

    Add files via upload

--- Vincenzo/30Settembre2020/database/src/database/test01/FirstConnection.java
+++ Vincenzo/30Settembre2020/database/src/database/test01/FirstConnection.java
@@ -1,11 +1,37 @@
 package database.test01;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class FirstConnection {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		String driver = "org.mariadb.jdbc.Driver";
-		System.out.println(driver);
-
+		
+		Class.forName(driver);
+		
+		Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/master?user=master&password=master");
+		
+		String query = "select id, nome from regioni";
+		
+		Statement st = connection.createStatement();
+		
+		ResultSet rs = st.executeQuery(query);
+		
+		while(rs.next()) {
+			System.out.print("id=");
+			System.out.print(rs.getInt("id"));
+			System.out.print(" ");
+			System.out.print("regione=");
+			System.out.println(rs.getString(2));
+		}
+		
+		rs.close();
+		st.close();
+		connection.close();
 	}
 
 }
