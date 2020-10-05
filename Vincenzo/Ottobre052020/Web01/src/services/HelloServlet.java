e2c36e436ddec72cb93a6fa660a5304c13a9bbc0
Vincenzo <v.santoro21@studenti.unisa.it>
2020-10-05 12:59:36 +0200

    Add files via upload

--- Vincenzo/Ottobre052020/Web01/src/services/HelloServlet.java
+++ Vincenzo/Ottobre052020/Web01/src/services/HelloServlet.java
@@ -1,6 +1,8 @@
 package services;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,16 +26,21 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		process("GET", request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		process("POST", request, response);
+	}
+
+	protected void process(String metodo, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.append(metodo);
+		pw.append(" ===> Hello l'applicazione sta funzionando");
+		pw.append("GET Served at: ").append(request.getContextPath());
 	}
 
 }
