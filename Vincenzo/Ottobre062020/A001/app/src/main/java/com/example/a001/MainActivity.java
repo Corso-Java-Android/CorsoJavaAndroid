571d607e40e3b624a025b395b1926f67ff955e71
Vincenzo <v.santoro21@studenti.unisa.it>
2020-10-06 17:06:43 +0200

    logcat

--- Vincenzo/Ottobre062020/A001/app/src/main/java/com/example/a001/MainActivity.java
+++ Vincenzo/Ottobre062020/A001/app/src/main/java/com/example/a001/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.a001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public  MainActivity(){
+        super();
+        Log.d("***************** MainActivity()", "Costruttore in esecuzione");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("***************** onCreate()", "onCreate in esecuzione");
         setContentView(R.layout.activity_main);
     }
 }
