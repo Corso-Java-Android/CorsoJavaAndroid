0057540b32fcf7ec7d0e7b01a0aa0103cc6880f2
Vincenzo <v.santoro21@studenti.unisa.it>
2020-10-06 17:28:06 +0200

    onDestroy()

--- Vincenzo/Ottobre062020/A001/app/src/main/java/com/example/a001/MainActivity.java
+++ Vincenzo/Ottobre062020/A001/app/src/main/java/com/example/a001/MainActivity.java
@@ -7,15 +7,23 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String tag = "******** MainActivity()";
+
     public  MainActivity(){
         super();
-        Log.d("***************** MainActivity()", "Costruttore in esecuzione");
+        Log.d(tag, "Costruttore in esecuzione");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("***************** onCreate()", "onCreate in esecuzione");
+        Log.d(tag, "onCreate in esecuzione");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy(){
+        Log.d(tag, "onDestroy in esecuzione");
+        super.onDestroy();
     }
 }
